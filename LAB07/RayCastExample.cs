9ef17bad8269abb3ff37764cc7b86f6e645dc489
Grash <grashuriza@gmail.com>
2018-10-11 18:03:52 -0400

    Lab07

---
+++ LAB07/RayCastExample.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayCastExample : MonoBehaviour {
+    public bool CheckForAllHits = false;
+    public float CheckDistance = 50.0f; 
+    public bool HasFoundObject = false; 
+    public string FoundObject = "";
+    public Vector3 HitAtPoint = Vector3.zero;
+    public Vector3 HitAtNormal = Vector3.zero;
+    public List<string> FoundObjectList; 
+
+	void Update () {
+        if (CheckForAllHits)
+        {
+            MultipleHitCheck();
+        }
+        else
+        {
+            SingleHitCheck();        }
+	}
+
+   
+
+    public void SingleHitCheck()
+    {
+        Util_ResetEditorPublics();
+
+        RaycastHit hit;
+        bool hashit = Physics.Raycast(transform.position, transform.forward, out hit, CheckDistance); 
+        if (hashit)
+        {
+            Util_ProcessHit(hit);
+        }
+    }
+
+    public void MultipleHitCheck()
+    {
+        Util_ResetEditorPublics();
+        FoundObjectList.Clear(); 
+
+        RaycastHit[] hits;
+        hits = Physics.RaycastAll(transform.position, transform.forward, CheckDistance);
+
+        foreach (RaycastHit hit in hits)
+        {
+            Util_ProcessHit(hit);
+            FoundObjectList.Add(hit.collider.gameObject.name); 
+        }
+    }
+    public void Util_ResetEditorPublics()
+    {
+        HasFoundObject = false;
+        FoundObject = "";
+        HitAtPoint = Vector3.zero;
+        HitAtNormal = Vector3.zero;
+    }
+
+    public void Util_ProcessHit(RaycastHit hit)
+    {
+        HasFoundObject = true;
+        FoundObject = hit.collider.gameObject.name;
+        HitAtPoint = hit.point;
+        HitAtNormal = hit.normal;
+    }
+}
---
+++ LAB07/RayExample.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class RayExample : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        RayExampleCode();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DrawDebugRay();
+    }
+
+    public void RayExampleCode()
+    {
+        Ray rayExample = new Ray();
+        rayExample.origin = Vector3.zero;
+        // aka (0,0,0)
+        rayExample.direction = Vector3.forward;
+        // aka (0,0,1)
+        Vector3 point = rayExample.GetPoint(13);
+        // aka 13 * (0,0,1) or (0,0,13)
+        Debug.Log("My point 13 units on the ray is " + point.ToString());
+    }
+
+    public void DrawDebugRay()
+    {
+        float drawLength = 25.0f;
+        Vector3 origin = gameObject.transform.position;
+        Vector3 direction = origin + (gameObject.transform.forward * drawLength); ;
+        Color drawColor = Color.red;
+
+        Debug.DrawRay(origin, direction, drawColor);
+    }
+}
